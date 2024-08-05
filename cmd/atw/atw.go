2bd195867a83ff6358f618c1b726b628b6b3123a
Dan Taylor <94151520+mortedecai@users.noreply.github.com>
2024-08-04 22:03:22 -0400

    Initial Setup (#1)
    * Initial setup with command line structure
    * Initial build setup

---
+++ cmd/atw/atw.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"os"
+)
+
+type atwApp struct {
+	// TODO: {mortedecai} Move this to a more appropriate location in future.
+
+	out *os.File
+}
+
+func New(out *os.File) *atwApp {
+	return &atwApp{out: out}
+}
+
+func (atw *atwApp) Write(msg string) (n int, err error) {
+	// TODO: {mortedecai} Delete this once more testable code is in place.
+	return atw.out.WriteString(msg)
+}
+
+func main() {
+	atw := New(os.Stdout)
+	atw.Write("Hello\n")
+}
